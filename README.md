d9a803e874ee3cb215ec45525ce4c5b83238dfd2
Ana Gelez <ana@gelez.xyz>
2020-08-11 18:49:27 +0200

    Zola-ify
    Bye bye Rust and TypeScript

--- README.md
+++
@@ -1,3 +0,0 @@
-# shef.top
-Un super site
-C'est coule carre honvas fer pl1 2 truks apré
--- front/src/index.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-const appContainer = window.document.querySelector('#app');
-ReactDOM.render(React.createElement(App), appContainer);
